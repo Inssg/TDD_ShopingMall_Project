fe9c7e6b0648b3ef9d6038304e30ac42b27ef916
Inssg <nightwalker0131@naver.com>
2023-02-16 18:29:38 +0900

    feat: Item Entity 클래스 추가

--- backend/src/test/java/org/inssg/backend/item/ItemServiceTest.java
+++ backend/src/test/java/org/inssg/backend/item/ItemServiceTest.java
@@ -7,6 +7,8 @@ import org.springframework.util.Assert;
 
 import javax.swing.*;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ItemServiceTest {
@@ -19,11 +21,11 @@ public class ItemServiceTest {
         itemService = new ItemService();
     }
 
-    //TODO상품조회리스트 테스트 구현
+    //TODO 상품조회리스트 테스트 구현
     @Test
-    void 상품조회() {
+    void 상품리스트조회() {
         //상품조회
-        ItemResponse response = itemService.getList();
+        List<ItemResponse> response = itemService.getList();
         //상품검증
         assertThat(response).isNotNull();
     }
@@ -43,9 +45,30 @@ public class ItemServiceTest {
     }
 
     private class ItemService {
+        //TODO: ItemRepository에서 item List 뽑아와서 ItemResponse List로 변경
+        //TODO: ItemRepository findAll -> findList (페이지네이션해서 조회로 변경필요)
+        public List<ItemResponse> getList() {
+            List<Item> item = itemRepository.findAll();
 
-        public ItemResponse getList() {
-            return null;
+            return List.of(
+                   new ItemResponse(1L,"프로틴","https://unsplash.com/2",50000),
+                   new ItemResponse(2L,"닭가슴살","https://unsplash.com/1",30000),
+                   new ItemResponse(3L,"밀크시슬","https://unsplash.com/3",20000)
+            );
+        }
+    }
+
+    private class Item {
+        private Long id;
+        private String name;
+        private String imgPath;
+        private int price;
+
+        public Item(Long id, String name, String imgPath, int price) {
+            this.id = id;
+            this.name = name;
+            this.imgPath = imgPath;
+            this.price = price;
         }
     }
 }
