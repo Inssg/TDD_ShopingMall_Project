5f45fbff67fc99ad647acaf262b07ea4e6ba8d89
Inssg <nightwalker0131@naver.com>
2023-02-16 15:58:28 +0900

    Set: 초기세팅

---
+++ backend/src/test/java/org/inssg/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inssg.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
