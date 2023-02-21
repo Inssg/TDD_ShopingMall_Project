2c00d9a537c71ad90f303dfbb941cf01e41388b1
Inssg <nightwalker0131@naver.com>
2023-02-21 15:49:04 +0900

    feat: 인증 성공, 실패 핸들러 추가

---
+++ backend/src/main/java/org/inssg/backend/security/handler/MemberAuthenticationFailureHandler.java
@@ -0,0 +1,35 @@
+package org.inssg.backend.security.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.inssg.backend.error.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class MemberAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.error("# Authentication failed: {}", exception.getMessage());
+
+        sendErrorResponse(response);
+    }
+
+    private void sendErrorResponse(HttpServletResponse response) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(mapper.writeValueAsString(errorResponse));
+    }
+}
