6c3ff49c6f504d3058b6a86cd088edd18cfe8de4
Inssg <nightwalker0131@naver.com>
2023-03-12 01:46:38 +0900

    test: 데이터 베이스 초기화 설정

---
+++ backend/src/test/java/org/inssg/backend/util/DatabaseCleanup.java
@@ -0,0 +1,67 @@
+package org.inssg.backend.util;
+
+import com.google.common.base.CaseFormat;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Table;
+import javax.persistence.metamodel.EntityType;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class DatabaseCleanup implements InitializingBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<String> tableNames;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        tableNames = entities.stream()
+                .filter(e -> isEntity(e) && hasTableAnnotation(e) )
+                .map(e -> {
+                    String tableName = e.getJavaType().getAnnotation(Table.class).name();
+                    return tableName.isBlank() ? CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, e.getName()) : tableName;
+                })
+                .collect(Collectors.toList());
+
+        List<String> entityNames = entities.stream()
+                .filter(e -> isEntity(e) && !hasTableAnnotation(e))
+                .map(e -> CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, e.getName()))
+                .collect(Collectors.toList());
+
+        tableNames.addAll(entityNames);
+
+    }
+
+    private boolean hasTableAnnotation(EntityType<?> e) {
+        return e.getJavaType().getAnnotation(Table.class) != null;
+    }
+
+    @Transactional
+    public void execute() {
+        entityManager.flush();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN  ID RESTART  WITH 1").executeUpdate();
+        }
+
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+
+    }
+
+    private boolean isEntity(EntityType<?> e) {
+        return e.getJavaType().getAnnotation(Entity.class) != null;
+    }
+
+}
